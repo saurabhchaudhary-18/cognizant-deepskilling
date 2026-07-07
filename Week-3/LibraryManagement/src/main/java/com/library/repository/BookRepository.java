547f27c84e23cbc62973634e16ec1dea32558cf7
saurabhchaudhary-18 <saurabh1234singh123@gmail.com>
2026-07-07 12:45:53 +0530

    Add implementation for Week-3 LibraryManagement

---
+++ Week-3/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,13 @@
+
+package com.library.repository;
+
+public class BookRepository {
+
+    public BookRepository() {
+        System.out.println("BookRepository bean created.");
+    }
+
+    public void displayRepository() {
+        System.out.println("Book Repository is working.");
+    }
+}

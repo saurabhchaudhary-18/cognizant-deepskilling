612e5f25fe88e0a861e6bbb37c379fdfbb5e1a22
saurabhchaudhary-18 <saurabh1234singh123@gmail.com>
2026-07-07 13:22:06 +0530

    Exercise 5 Week3 added

---
+++ Week-3/LibManagement/src/main/java/com/library/Application.java
@@ -0,0 +1,9 @@
+
+package com.library;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Library Management Application Started Successfully!");
+    }
+}

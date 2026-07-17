42b4d06dbb4052ce4373c99f507cebc1a52206bd
saurabhchaudhary-18 <saurabh1234singh123@gmail.com>
2026-07-17 21:30:02 +0530

    Week5 added

---
+++ Week-5/account/src/main/java/controller/AccountController.java
@@ -0,0 +1,22 @@
+
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.account.model.Account;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Account getAccount(@PathVariable String number) {
+
+        return new Account(
+                "00987987973432",
+                "savings",
+                234343
+        );
+    }
+}
